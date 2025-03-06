768e61b797bd9cb26fd8ea2d24543a9acdf96f63
ItouKotaro <231019@st.yoshida-g.ac.jp>
2025-03-06 10:57:24 +0900

    入力に関するシステムを追加しました

--- src/system/input.h
+++ src/system/input.h
@@ -6,6 +6,27 @@
 #ifndef _INPUT_H_
 #define _INPUT_H_
 
+#include "KeyTables.h"
+
+//@brief 入力デバイス（ベース）
+class InputDevice
+{
+public:
+	//@brief 初期化
+	virtual void Init(HINSTANCE hInstance, HWND hwnd) = 0;
+
+	//@brief 終了
+	virtual void Uninit() = 0;
+
+	//@brief 更新
+	virtual void Update() = 0;
+
+	//@brief 入力情報の取得
+	virtual bool GetKey(const std::string&) { return false; }
+};
+
+
+
 //@brief 入力マネージャー
 class InputManager final
 {
@@ -19,8 +40,14 @@ public:
 	//@brief 終了
 	void Uninit();
 
+	//@brief 更新
+	void Update();
+
 private:
-	LPDIRECTINPUT8 m_input;
+	LPDIRECTINPUT8 m_input;						// 入力
+	std::vector<InputDevice*> m_devices;	// 入力デバイス
+	std::vector<KeyCode> m_keyData;			// キーデータ
+	std::vector<KeyCode> m_oldKeyData;	// 前回のキーデータ
 };
 
 
